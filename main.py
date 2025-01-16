54be5b699b8192060bd700d3046fc0ff608a185e
praveen <praveen.gopi717@gmail.com>
2025-01-16 13:03:08 +0530

    stop tracking env

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ app=FastAPI()
 load_dotenv()
 # AWS S3 Configuration
 
-'''
+
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-'''
 
-'''
-# Initialize S3 Client
+
+
+# Initalize S3 Client
 s3_client = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name=AWS_REGION,
 )
-'''
+
 
 @app.get("/upload")
 def upload():
